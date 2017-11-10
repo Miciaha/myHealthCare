f9dfd06cadcab39905c98d8c259bdf2e63b80378
Miciaha Ivey <mdi13b@my.fsu.edu>
2017-11-10 16:47:50 -0500

    Added Health Overview section

--- js/charts-home.js
+++ js/charts-home.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     'use strict';
 
     // Main Template Color
-    var brandPrimary = '#33b35a';
+    var brandPrimary = '#3185fc';
 
 
     // ------------------------------------------------------- //
@@ -19,7 +19,7 @@ $(document).ready(function () {
             }
         },
         data: {
-            labels: ["Jan", "Feb", "Mar", "Apr", "May", "June", "July"],
+            labels: ["Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun"],
             datasets: [
                 {
                     label: "My First dataset",
@@ -41,30 +41,7 @@ $(document).ready(function () {
                     pointHoverBorderWidth: 2,
                     pointRadius: 1,
                     pointHitRadius: 0,
-                    data: [50, 20, 60, 31, 52, 22, 40],
-                    spanGaps: false
-                },
-                {
-                    label: "My First dataset",
-                    fill: true,
-                    lineTension: 0.3,
-                    backgroundColor: "rgba(75,192,192,0.4)",
-                    borderColor: "rgba(75,192,192,1)",
-                    borderCapStyle: 'butt',
-                    borderDash: [],
-                    borderDashOffset: 0.0,
-                    borderJoinStyle: 'miter',
-                    borderWidth: 1,
-                    pointBorderColor: "rgba(75,192,192,1)",
-                    pointBackgroundColor: "#fff",
-                    pointBorderWidth: 1,
-                    pointHoverRadius: 5,
-                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                    pointHoverBorderColor: "rgba(220,220,220,1)",
-                    pointHoverBorderWidth: 2,
-                    pointRadius: 1,
-                    pointHitRadius: 10,
-                    data: [65, 59, 30, 81, 46, 55, 30],
+                    data: [4, 5, 8, 5, 6],
                     spanGaps: false
                 }
             ]
